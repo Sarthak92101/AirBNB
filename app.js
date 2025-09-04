c6091c2dab0635bd8714afb1d3a4ca6de53d2b63
Sarthak Sharma <sarthaksharma25702@gmail.com>
2025-09-04 20:57:40 +0530

    Add files

--- app.js
+++ app.js
@@ -1,32 +1,57 @@
-const express=require("express");
-const app=express();
-const mongoose=require("mongoose");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const { listen } = require("express/lib/application");
-const path=require("path")
+const path = require("path")
 
-const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
-main().then(()=>{
+main().then(() => {
   console.log("connected to DB")
-}).catch(err=>{
+}).catch(err => {
   console.log(err);
 })
-async function main(){
+async function main() {
   await mongoose.connect(MONGO_URL);
 }
 
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"))
-app.get("/",(req,res)=>{
-res.send("Hii, I am Root ")
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"))
+app.use(express.urlencoded({ extended: true }));
+
+
+
+app.get("/", (req, res) => {
+  res.send("Hii, I am Root ")
 })
 
 //Index Route
-app.get("/listings",async(req,res)=>{
-const alllistings=await Listing.find({});
-res.render("listings/index.ejs", {alllistings})
+app.get("/listings", async (req, res) => {
+  const alllistings = await Listing.find({});
+  res.render("listings/index.ejs", { alllistings })
 });
+
+//New Route
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new")
+})
+
+//Show route
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  id = id.trim();
+  const listing = await Listing.findById(id);
+  res.render("listings/show", { listing })
+})
+
+//Create Route
+app.post("/listings", async (req, res) => {
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings")
+})
+
 
 
 // app.get("/testlisting",async(req,res)=>{
@@ -45,6 +70,6 @@ res.render("listings/index.ejs", {alllistings})
 
 
 
-app.listen(8080,()=>{
+app.listen(8080, () => {
   console.log("Server is listeining to port 8080");
 })
