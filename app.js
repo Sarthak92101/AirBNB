62842819c27f688cf328956813bb962a8034f40c
Sarthak Sharma <sarthaksharma25702@gmail.com>
2025-10-07 15:11:00 +0530

    New

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsyncr.js");
 const ExpressError = require("./utils/ExpressError.js");
+const { listingschema } = require("./schema.js")
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -33,6 +34,16 @@ app.get("/", (req, res) => {
   res.send("Hii, I am Root ")
 })
 
+const validatelisting = (req, res, next) => {
+  let {error} = listingschema.validate(req.body);
+  if (error) {
+    let errMsg=error.details.map((el)=>el.message ).join(",")
+    throw new ExpressError(400, errMsg )
+  }else{
+    next();
+  }
+}
+
 //Index Route
 app.get("/listings", wrapAsync(async (req, res) => {
   const alllistings = await Listing.find({});
@@ -53,30 +64,24 @@ app.get("/listings/:id", wrapAsync(async (req, res) => {
 }));
 
 //Create Route
-app.post("/listings", wrapAsync(async (req, res, next) => {
-  if(!req.body.listings){
-    throw new ExpressError(400,"Send valid data for listing")
-  }
+app.post("/listings",validatelisting, wrapAsync(async (req, res, next) => {
   const newListing = new Listing(req.body.listing);
   await newListing.save();
-  res.redirect(`/listings/${newListing._id}`)
+  res.redirect("/listings")
 })
 );
 
 //Edit Route
-app.get("/listings/:id/edit",wrapAsync( async (req, res) => {
+app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
   res.render("listings/edit.ejs", { listing })
 }))
 
 //Update route
-app.put("/listings/:id", wrapAsync(async (req, res) => {
-  if(!req.body.listings){
-    throw new ExpressError(400,"Send valid data for listing")
-  }
+app.put("/listings/:id", validatelisting, wrapAsync(async (req, res) => {
   let { id } = req.params;
-  await Listing.findByIdAndUpdate(id,{ ...req.body.listing });
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
   res.redirect("/listings");
 }))
 
@@ -109,7 +114,7 @@ app.delete("/listings/:id", wrapAsync(async (req, res) => {
 // })
 
 app.use((err, req, res, next) => {
-  let {statusCode=500,message="Something went wrong"} =  err;
+  let { statusCode = 500, message = "Something went wrong" } = err;
   res.status(statusCode).render("error.ejs", { err });
 })
 
