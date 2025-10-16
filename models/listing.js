c8e62b130d1a7dd7ff21a310a7bdc4bfa45c62a3
Sarthak Sharma <sarthaksharma25702@gmail.com>
2025-10-16 11:28:25 +0530

    New

--- models/listing.js
+++ models/listing.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const review = require("./review");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
@@ -14,16 +15,20 @@ const listingSchema = new Schema({
       default: "default-image"
     },
     url: {
-  type: String,
-  default: "https://images.unsplash.com/photo-1505691938895-1758d7feb511?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80"
-}
-
-
+      type: String,
+      default: "https://images.unsplash.com/photo-1505691938895-1758d7feb511?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80"
+    }
   },
 
   price: Number,
   location: String, // corrected typo
   country: String,
+  reviews:[
+    {
+      type:Schema.Types.ObjectId,
+      ref:"Review"
+    }
+  ]
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
